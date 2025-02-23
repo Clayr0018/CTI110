63be5516b1527a8dfc835435363bf13c4456d58d
Clayr0018 <clayr0018@student.faytechcc.edu>
2025-02-22 20:13:02 -0500

    Add files via upload
    Added P1LAB1 welcome message program

---
+++ P1LAB1CLAY_RIKIRA.py
@@ -0,0 +1,13 @@
+# Rikira Clay
+# Feb 22, 2025
+# P1LAB1
+# This program asks for the user's first and last name and displays a welcome message.
+
+# Get the user's first name
+first_name = input("Rikira: ")
+
+# Get the user's last name
+last_name = input("Clay: ")
+
+# Display the welcome message
+print(f"Hello, {Rikira} {Clay}! Welcome to CTI-110")
